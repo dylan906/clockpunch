dc658f4a59f0379fb813b84bdbb381a992ad7c72
Dylan Penn <dylanrpenn@vt.edu>
2023-07-10 13:46:24 -0400

    making MWE for #3

--- issues/iss3.py
+++ issues/iss3.py
@@ -1,16 +1,18 @@
 """Script to recreate issue #3."""
 # %% Imports
+# Standard Library Imports
+from copy import deepcopy
+
 # Third Party Imports
-import gymnasium as gym
 import ray
 from ray import air, tune
-from ray.rllib.algorithms import ppo
+from ray.rllib.models import ModelCatalog
 from ray.tune.registry import register_env
 
 # Punch Clock Imports
 from punchclock.common.utilities import loadJSONFile
+from punchclock.nets.action_mask_model import MyActionMaskModel
 from punchclock.ray.build_env import buildEnv
-from punchclock.ray.build_tuner import buildTuner
 
 # %% Load config json
 config = loadJSONFile("issues/iss3_config.json")
@@ -20,32 +22,23 @@ register_env("my_env", buildEnv)
 ModelCatalog.register_custom_model("action_mask_model", MyActionMaskModel)
 
 # %% Assemble inputs for tune.Tuner()
-
-# run_config = air.RunConfig(
-#     **config["run_config"],
-# )
-run_config = air.RunConfig(
-    name="exp_name",
-    local_dir="tests/ray",
-    stop={"timesteps_total": 10},
-)
-tune_config = tune.tune_config.TuneConfig(**config["tune_config"])
-
-print(config["param_space"])
-param_space = config["param_space"].update(
+param_space = deepcopy(config["param_space"])
+param_space.update(
     {
         "framework": "torch",
-        "env": "SSAScheduler",
+        "env": "my_env",
         "horizon": None,  # environment has its own horizon
-        "ignore_worker_failures": True,
-        "log_level": "DEBUG",
-        "num_gpus": 0,
     }
 )
-print(param_space)
 
+
+run_config = air.RunConfig(
+    stop={"timesteps_total": 10},
+)
+tune_config = tune.tune_config.TuneConfig()
+
+# %% Make tuner
 ray.init(num_cpus=16)
-
 # tuner = buildTuner(config)
 tuner = tune.Tuner(
     trainable="PPO",
