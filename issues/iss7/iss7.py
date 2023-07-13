24e6cc50eab46f0c8d2b3876ea4db4d0e9db3b2c
Dylan Penn <dylanrpenn@vt.edu>
2023-07-13 16:54:22 -0400

    iss7 script and test config #7

---
+++ issues/iss7/iss7.py
@@ -0,0 +1,7 @@
+"""Issue 7 reproduction script."""
+# %% Imports
+# Punch Clock Imports
+from punchclock.common.utilities import loadJSONFile
+
+# %% Load config
+config = loadJSONFile("issues/iss7/iss7_config.json")
