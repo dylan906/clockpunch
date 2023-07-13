8b15e0ec5ab22d01f17dc2f29ce9fbc0cfb3b137
Dylan Penn <dylanrpenn@vt.edu>
2023-07-13 17:53:33 -0400

    working on #7

--- issues/iss7/iss7.py
+++ issues/iss7/iss7.py
@@ -19,43 +19,47 @@ config = loadJSONFile("issues/iss7/iss7_config.json")
 register_env("my_env", buildEnv)
 ModelCatalog.register_custom_model("action_mask_model", MyActionMaskModel)
 
-# build test env
-# algo_config = (
-#     ppo.PPOConfig()
-#     .training(model={**config["param_space"]["model"]})
-#     .environment(
-#         env="my_env",
-#         env_config=config["param_space"]["env_config"],
-#     )
-#     .framework("torch")
-# )
-# env = buildEnv(config["param_space"]["env_config"])
-env = RandomEnv(
-    {
-        "observation_space": Dict(
-            {
-                "observations": Box(0, 1, shape=(3,)),
-                "action_mask": Box(0, 1, shape=(3,), dtype=int),
-            }
-        ),
-        "action_space": MultiDiscrete([3]),
-    }
-)
-# build algo
+# Custom Env
 algo_config = (
     ppo.PPOConfig()
-    .training(
-        model={**config["param_space"]["model"]},
-    )
+    .training(model={**config["param_space"]["model"]})
     .environment(
-        env=RandomEnv,
-        env_config={
-            "observation_space": env.observation_space,
-            "action_space": env.action_space,
-        },
+        env="my_env",
+        env_config=config["param_space"]["env_config"],
     )
     .framework("torch")
 )
+env = buildEnv(config["param_space"]["env_config"])
+
+# Random Env
+# env = RandomEnv(
+#     {
+#         "observation_space": Dict(
+#             {
+#                 "observations": Box(0, 1, shape=(3,)),
+#                 "action_mask": Box(0, 1, shape=(3,), dtype=int),
+#             }
+#         ),
+#         "action_space": MultiDiscrete([3]),
+#     }
+# )
+
+# # build algo
+# algo_config = (
+#     ppo.PPOConfig()
+#     .training(
+#         model={**config["param_space"]["model"]},
+#     )
+#     .environment(
+#         env=RandomEnv,
+#         env_config={
+#             "observation_space": env.observation_space,
+#             "action_space": env.action_space,
+#         },
+#     )
+#     .framework("torch")
+# )
+
 algo = algo_config.build()
 
 # test algo
