be4249c23f7ee1f773475c4f6d6ff6d8c84850fb
Dylan Penn <dylanrpenn@vt.edu>
2023-07-17 09:40:16 -0400

    reorganizing and clarifying variable names #7

--- issues/iss7/iss7.py
+++ issues/iss7/iss7.py
@@ -2,17 +2,14 @@
 # %% Imports
 # Third Party Imports
 from gymnasium.spaces import Box, Dict, MultiDiscrete
-from ray.air import CheckpointConfig, RunConfig
-from ray.air.config import FailureConfig
+from ray.air import RunConfig
 from ray.rllib.algorithms import ppo
 from ray.rllib.examples.env.random_env import RandomEnv
 from ray.rllib.models import ModelCatalog
-from ray.tune import Tuner
 from ray.tune.registry import register_env
 
 # Punch Clock Imports
 from punchclock.common.utilities import loadJSONFile
-from punchclock.environment.wrappers import ActionMask
 from punchclock.nets.action_mask_model import MyActionMaskModel
 from punchclock.ray.build_tuner import buildEnv
 
@@ -23,22 +20,44 @@ config = loadJSONFile("issues/iss7/iss7_config.json")
 register_env("my_env", buildEnv)
 ModelCatalog.register_custom_model("action_mask_model", MyActionMaskModel)
 
-# %% Modify config
+# %% Modify config (custom env)
+run_config = RunConfig(**config["run_config"])
 # Disable preprocessor
-# config["param_space"]["model"]["_disable_preprocessor_api"] = True
-run_config = RunConfig(
-    checkpoint_config=CheckpointConfig(
-        num_to_keep=2,
-        checkpoint_score_attribute="episode_reward_mean",
-        checkpoint_at_end=True,
-    ),
-    failure_config=FailureConfig(max_failures=2),
-    # stop=stopper,
-    **config["run_config"],
+config["param_space"]["model"]["_disable_preprocessor_api"] = True
+
+# %% Random Env
+env_random = RandomEnv(
+    {
+        "observation_space": Dict(
+            {
+                "observations": Box(0, 1, shape=(32,), dtype=float),
+                "action_mask": Box(0, 1, shape=(10,), dtype=int),
+            }
+        ),
+        "action_space": MultiDiscrete([10]),
+    }
+)
+algo_config_rand = (
+    ppo.PPOConfig()
+    .training(
+        model={**config["param_space"]["model"]},
+    )
+    .environment(
+        env=RandomEnv,
+        env_config={
+            "observation_space": env_random.observation_space,
+            "action_space": env_random.action_space,
+        },
+    )
+    .framework("torch")
 )
 
-# Custom Env
-algo_config = (
+algo_random = algo_config_rand.build()
+results = algo_random.training_step()
+print(f"random env results : \n{results}")
+
+# %% Custom Env
+algo_config_customenv = (
     ppo.PPOConfig()
     .training(model={**config["param_space"]["model"]})
     .environment(
@@ -47,48 +66,24 @@ algo_config = (
     )
     .framework("torch")
 )
-env = buildEnv(config["param_space"]["env_config"])
+algo_customenv = algo_config_customenv.build()
 
-# Random Env
-# env = RandomEnv(
-#     {
-#         "observation_space": Dict(
-#             {
-#                 "observations": Box(0, 1, shape=(36,), dtype=float),
-#                 "action_mask": Box(0, 1, shape=(10,), dtype=int),
-#             }
-#         ),
-#         "action_space": MultiDiscrete([10]),
-#     }
+try:
+    results = algo_customenv.training_step()
+    print(f"custom env results : \n{results}")
+except Exception as e:
+    print(e)
+
+# env = buildEnv(config["param_space"]["env_config"])
+# obs = env.observation_space.sample()
+# action = algo.compute_single_action(obs)
+
+# tuner = Tuner(
+#     trainable="PPO",
+#     param_space=config["param_space"],
+#     run_config=run_config,
+#     tune_config=config["tune_config"],
 # )
-# algo_config = (
-#     ppo.PPOConfig()
-#     .training(
-#         model={**config["param_space"]["model"]},
-#     )
-#     .environment(
-#         env=RandomEnv,
-#         env_config={
-#             "observation_space": env.observation_space,
-#             "action_space": env.action_space,
-#         },
-#     )
-#     .framework("torch")
-# )
-
-algo = algo_config.build()
-
-# test algo
-obs = env.observation_space.sample()
-action = algo.compute_single_action(obs)
-# results = algo.training_step()
-
-tuner = Tuner(
-    trainable="PPO",
-    param_space=config["param_space"],
-    run_config=run_config,
-    tune_config=config["tune_config"],
-)
-tuner.fit()
+# tuner.fit()
 # %% done
 print("done")
