1e9155223da817ded656ea788a203e4c1fb3cd2f
Dylan Penn <dylanrpenn@vt.edu>
2023-08-31 15:20:50 -0400

    refactored random policy to use MultiBinary 2d action mask #37

--- punchclock/policies/random_policy.py
+++ punchclock/policies/random_policy.py
@@ -4,11 +4,12 @@ from __future__ import annotations
 
 # Third Party Imports
 import gymnasium as gym
-from gymnasium.spaces import Box
-from gymnasium.spaces.utils import flatten, flatten_space, unflatten
-from numpy import int64, ndarray, zeros
+from gymnasium.spaces import MultiBinary
+from gymnasium.spaces.utils import flatten
+from numpy import ndarray, zeros
 
 # Punch Clock Imports
+from punchclock.common.utilities import MaskConverter
 from punchclock.policies.action_selection import epsGreedyMask
 from punchclock.policies.policy_base_class_v2 import CustomPolicy
 
@@ -45,37 +46,18 @@ class RandomPolicy(CustomPolicy):
         # of available actions for all sensors.
         self.num_targets = self.action_space.nvec[0] - 1
 
-        # Set some parameters only if using action mask.
-        if use_mask is True:
-            # mask_1d_space is 1d Box; action space is MD
-            self.mask_1d_space = flatten_space(self.action_space)
-
-            # 2d mask space is transpose of usual format; this is for easier
-            # transformation of actions in computeAction(). 2d mask is [M, (N+1)].
-            self.mask_2d_space = Box(
-                0,
-                2,
-                shape=(
-                    self.num_sensors,
-                    self.num_targets + 1,
-                ),
-                dtype=int64,
-            )
-
-            assert (
-                "action_mask" in self.observation_space.sample()
-            ), "The key 'action_mask' must be in environment observation space."
-        else:
-            self.mask_1d_space = None
-            self.mask_2d_space = None
+        self.mask_converter = MaskConverter(self.num_targets, self.num_sensors)
+        self.action_mask_space = MultiBinary(
+            (self.num_targets + 1, self.num_sensors)
+        )
 
     def computeAction(self, obs: dict) -> ndarray[int]:
         """Compute a single action from a single observation.
 
         Args:
             obs (`dict`): A single observation. If action masking is enabled, obs
-                must contain "action_mask" as a key. Action mask is a ( (N+1) * M, )
-                binary array, representing the flattened action space.
+                must contain "action_mask" as a key. Action mask is a ( N+1, M)
+                binary array.
 
         Returns:
             `ndarray[int]`: (M, ) MultiDiscrete actions valued 0-N denoting actions.
@@ -86,17 +68,16 @@ class RandomPolicy(CustomPolicy):
         if self.use_mask is False:
             mask = None
         else:
-            mask_flat = obs["action_mask"]
-            # Convert mask from 1d array to 2d array
-            mask = unflatten(self.mask_2d_space, mask_flat).transpose()
+            mask = obs["action_mask"]
+            # # Convert mask from 1d array to 2d array
+            # mask = unflatten(self.action_mask_space, mask_flat).transpose()
 
         action = epsGreedyMask(Q=Q, epsilon=1, mask=mask)
         action_flat = flatten(self.action_space, action)
+        action_2d = self.mask_converter.convertActionMaskFrom1dTo2d(action_flat)
 
         if self.use_mask is True:
             # Make sure actions don't violate action mask
-            assert (
-                action_flat <= mask_flat
-            ).all(), "Action violates action mask."
+            assert (action_2d <= mask).all(), "Action violates action mask."
 
         return action
--- tests/policies/test_random_policy.py
+++ tests/policies/test_random_policy.py
@@ -1,27 +1,26 @@
 """Test for random_policy.py."""
 # %% Imports
 # Third Party Imports
-from gymnasium.spaces import Box, Dict, MultiDiscrete
+from gymnasium.spaces import Box, Dict, MultiBinary, MultiDiscrete
 from numpy import Inf
 
 # Punch Clock Imports
+from punchclock.common.utilities import MaskConverter
 from punchclock.policies.random_policy import RandomPolicy
 
 # %% Build action/obs spaces
 num_sensors = 2
 num_targets = 4
-mask_dim = num_sensors * (num_targets + 1)
+mask_dim = (num_targets + 1, num_sensors)
 obs_space = Dict(
     {
         "observations": Dict(
             {
                 "est_cov": Box(-Inf, Inf, shape=[6, num_targets], dtype=float),
-                "vis_map_est": Box(
-                    0, 1, shape=[num_targets, num_sensors], dtype=int
-                ),
+                "vis_map_est": MultiBinary((num_targets, num_sensors)),
             }
         ),
-        "action_mask": Box(0, 1, shape=[mask_dim], dtype=int),
+        "action_mask": MultiBinary(mask_dim),
     }
 )
 
@@ -54,9 +53,7 @@ obs_space = Dict(
         "observations": Dict(
             {
                 "est_cov": Box(-Inf, Inf, shape=[6, num_targets], dtype=float),
-                "vis_map_est": Box(
-                    0, 1, shape=[num_targets, num_sensors], dtype=int
-                ),
+                "vis_map_est": MultiBinary((num_targets, num_sensors)),
             }
         )
     }
@@ -77,12 +74,10 @@ obs_space = Dict(
         "observations": Dict(
             {
                 "est_cov": Box(-Inf, Inf, shape=[6, num_targets], dtype=float),
-                "vis_map_est": Box(
-                    0, 1, shape=[num_targets, num_sensors], dtype=int
-                ),
+                "vis_map_est": MultiBinary((num_targets, num_sensors)),
             }
         ),
-        "action_mask": Box(0, 1, shape=[mask_dim], dtype=int),
+        "action_mask": MultiBinary(mask_dim),
     }
 )
 pol = RandomPolicy(
@@ -98,10 +93,14 @@ print(f"action = {action}")
 
 # Test random actions in loop to make sure they don't violate mask. There is a
 # mask violation checker built into computeAction(); any violations will be tripped
-# automatically.
+# automatically. Randomly sampling observation space doesn't guarantee that vis_map
+# is consistent with action mask, so manually modify with a mask_converter.
+mask_converter = MaskConverter(num_targets, num_sensors)
 for _ in range(10):
-    # [obs, _, _, _] = env_masked.step(env_masked.action_space.sample())
     obs = obs_space.sample()
+    obs["action_mask"] = mask_converter.convert2dVisMaskTo2dActionMask(
+        obs["observations"]["vis_map_est"]
+    )
     vis_map = obs["observations"]["vis_map_est"]
     action = pol.computeAction(obs=obs)
     print(f"vis_map = {vis_map}")
