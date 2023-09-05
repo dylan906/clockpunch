56c459a32d397fc80e56eb7bec626b63e72be9fd
Dylan Penn <dylanrpenn@vt.edu>
2023-09-05 17:47:24 -0400

    added observation method #46

--- punchclock/environment/misc_wrappers.py
+++ punchclock/environment/misc_wrappers.py
@@ -1,5 +1,6 @@
 """Misc wrappers."""
 # %% Imports
+
 # Third Party Imports
 from gymnasium import Env, Wrapper
 
@@ -8,8 +9,14 @@ from gymnasium import Env, Wrapper
 class IdentityWrapper(Wrapper):
     """Wrapper does not modify environment, used for construction."""
 
+    # NOTE: SimRunner is hugely dependent on this class. Be careful about modifying
+    # it.
+
     def __init__(self, env: Env, id=None):
         super().__init__(env)
 
         self.id = id
         return
+
+    def observation(self, obs):
+        return obs
--- tests/environment/test_misc_wrappers.py
+++ tests/environment/test_misc_wrappers.py
@@ -11,7 +11,17 @@ from punchclock.environment.misc_wrappers import IdentityWrapper
 print("\nTest IdentityWrapper...")
 rand_env = RandomEnv({"observation_space": Box(0, 1)})
 identity_env = IdentityWrapper(rand_env)
+
 print(f"identity env = {identity_env}")
+unwrapped_obs = rand_env.observation_space.sample()
+wrapped_obs = identity_env.observation(unwrapped_obs)
+print(f"unwrapped obs = {unwrapped_obs}")
+print(f"wrapped obs = {wrapped_obs}")
+
+obs, reward, term, trunc, info = identity_env.step(
+    identity_env.action_space.sample()
+)
+
 
 identity_env = IdentityWrapper(rand_env, id="foo")
 print(f"identity env = {identity_env}")
