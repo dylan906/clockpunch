853053795193c860ba04db04ed539693d9f7422f
Dylan Penn <dylanrpenn@vt.edu>
2023-09-15 14:33:22 -0400

    tests for append info wrapper and randinfo wrapper #38 #57

--- tests/environment/test_misc_wrappers.py
+++ tests/environment/test_misc_wrappers.py
@@ -1,12 +1,17 @@
 """Tests for misc_wrappers.py."""
 # %% Imports
 # Third Party Imports
-from gymnasium.spaces import Box
+from gymnasium.spaces import Box, Dict, MultiDiscrete
 from ray.rllib.examples.env.random_env import RandomEnv
 
 # Punch Clock Imports
 from punchclock.common.agents import buildRandomAgent
-from punchclock.environment.misc_wrappers import IdentityWrapper, NumWindows
+from punchclock.environment.misc_wrappers import (
+    AppendInfoItemToObs,
+    IdentityWrapper,
+    NumWindows,
+    RandomInfo,
+)
 
 # %% Test IdentityWrapper
 print("\nTest IdentityWrapper...")
@@ -23,7 +28,6 @@ obs, reward, term, trunc, info = identity_env.step(
     identity_env.action_space.sample()
 )
 
-
 identity_env = IdentityWrapper(rand_env, id="foo")
 print(f"identity env = {identity_env}")
 print(f"env.id = {identity_env.id}")
@@ -37,6 +41,33 @@ rand_env.agents = agents
 rand_env.horizon = 10
 rand_env.time_step = 100
 
+# %% RandomInfo
+print("\nTest RandomInfo...")
+rand_env = RandomEnv()
+_, info_unwrapped = rand_env.reset()
+randinfo_env = RandomInfo(rand_env)
+_, _, _, _, info_wrapped = randinfo_env.step(randinfo_env.action_space.sample())
+print(f"unwrapped info = {info_unwrapped}")
+print(f"wrapped info = {info_wrapped}")
+
+randinfo_env = RandomInfo(rand_env, info_space=Dict({"a": Box(0, 1)}))
+_, _, _, _, info_wrapped = randinfo_env.step(randinfo_env.action_space.sample())
+print(f"wrapped info = {info_wrapped}")
+
+# %% AppendInfoItemToObs
+print("\nTest AppendInfoItemToObs...")
+rand_env = RandomInfo(
+    RandomEnv({"observation_space": Dict({"a": Box(0, 1)})}),
+    info_space=Dict({"b": MultiDiscrete(2)}),
+)
+appinfo_env = AppendInfoItemToObs(rand_env, info_key="b")
+obs, info = appinfo_env.reset()
+print(f"reset obs = {obs}")
+print(f"reset info = {info}")
+
+obs, _, _, _, info = appinfo_env.step(appinfo_env.action_space.sample())
+print(f"step obs = {obs}")
+print(f"step info = {info}")
 # %% Test NumWindows
 print("\nTest NumWindows...")
 nw_env = NumWindows(env=rand_env, use_estimates=False)
