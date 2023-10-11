f521a309f7d39908197363c8d0e83d2ad0adacf9
Dylan Penn <dylanrpenn@vt.edu>
2023-10-11 13:11:33 -0400

    filled out example #81

--- issues/iss81/custom_rnn_example.py
+++ issues/iss81/custom_rnn_example.py
@@ -1,14 +1,28 @@
 """Action Mask + LSTM Model."""
 # Custom RNN Example: https://github.com/ray-project/ray/blob/master/rllib/examples/models/rnn_model.py
 # %% Imports
+# Standard Library Imports
+import argparse
+import os
+
 # Third Party Imports
 import numpy as np
+import ray
+from ray import air, tune
+from ray.rllib.examples.env.repeat_after_me_env import RepeatAfterMeEnv
+from ray.rllib.examples.env.repeat_initial_obs_env import RepeatInitialObsEnv
+from ray.rllib.examples.models.rnn_model import RNNModel, TorchRNNModel
+from ray.rllib.models import ModelCatalog
 from ray.rllib.models.modelv2 import ModelV2
 from ray.rllib.models.preprocessors import get_preprocessor
 from ray.rllib.models.tf.recurrent_net import RecurrentNetwork
 from ray.rllib.models.torch.recurrent_net import RecurrentNetwork as TorchRNN
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune.registry import get_trainable_cls, register_env
+
+torch, nn = try_import_torch()
 
 
 # %% Custom RNN Class
@@ -76,3 +90,51 @@ class TorchRNNModel(TorchRNN, nn.Module):
         )
         action_out = self.action_branch(self._features)
         return action_out, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+
+
+if __name__ == "__main__":
+    ray.init()
+
+    ModelCatalog.register_custom_model("rnn", TorchRNNModel)
+    register_env("RepeatAfterMeEnv", lambda c: RepeatAfterMeEnv(c))
+    register_env("RepeatInitialObsEnv", lambda _: RepeatInitialObsEnv())
+
+    config = (
+        get_trainable_cls("PPO")
+        .get_default_config()
+        .environment("RepeatAfterMeEnv", env_config={"repeat_delay": 2})
+        .framework("torch")
+        .rollouts(num_rollout_workers=0, num_envs_per_worker=20)
+        .training(
+            model={
+                "custom_model": "rnn",
+                "max_seq_len": 20,
+                "custom_model_config": {
+                    "cell_size": 32,
+                },
+            },
+            gamma=0.9,
+            # TODO (Kourosh): Enable when LSTMs are supported.
+            _enable_learner_api=False,
+        )
+        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+        .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
+        .rl_module(_enable_rl_module_api=False)
+    )
+
+    config.training(entropy_coeff=0.001, num_sgd_iter=5, vf_loss_coeff=1e-5)
+
+    stop = {
+        "training_iteration": 1,
+        "timesteps_total": 100000,
+        "episode_reward_mean": 90.0,
+    }
+
+    tuner = tune.Tuner(
+        "PPO",
+        param_space=config.to_dict(),
+        run_config=air.RunConfig(stop=stop, verbose=1),
+    )
+    results = tuner.fit()
+
+    ray.shutdown()
