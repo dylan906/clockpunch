a3b0267e48e2235591f6dbf36ee96b21f9bf90fa
Dylan Penn <dylanrpenn@vt.edu>
2023-10-24 15:02:48 -0400

    pasting in example #90

--- punchclock/ray/stoppers.py
+++ punchclock/ray/stoppers.py
@@ -1,1 +1,20 @@
 """Custom Ray stoppers."""
+# %% Imports
+# Third Party Imports
+from ray import train, tune
+from ray.tune import Stopper
+
+
+# %% Classes
+class StopOnTrend(Stopper):
+    def __init__(self):
+        self.should_stop = False
+
+    def __call__(self, trial_id: str, result: dict) -> bool:
+        if not self.should_stop and result["mean_accuracy"] >= 0.8:
+            self.should_stop = True
+        return self.should_stop
+
+    def stop_all(self) -> bool:
+        """Returns whether to stop trials and prevent new ones from starting."""
+        return self.should_stop
--- tests/ray/test_stoppers.py
+++ tests/ray/test_stoppers.py
@@ -1,1 +1,18 @@
 """Tests for stoppers.py."""
+# %% Imports
+# Third Party Imports
+from ray import train, tune
+from ray.tune import Stopper
+
+# Punch Clock Imports
+from punchclock.ray.stoppers import StopOnTrend
+
+# %% Test StopOnTrend
+print("\nStopOnTrend...")
+stopper = StopOnTrend()
+tuner = tune.Tuner(
+    my_trainable,
+    run_config=train.RunConfig(stop=stopper),
+    tune_config=tune.TuneConfig(num_samples=2),
+)
+result_grid = tuner.fit()
