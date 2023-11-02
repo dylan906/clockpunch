af12436950c06e8fff327a5a77a5035e23e9e5c7
Dylan Penn <dylanrpenn@vt.edu>
2023-11-02 16:56:00 -0400

    building out usable classes for custody curriculum learner #96

--- punchclock/ray/curriculum.py
+++ punchclock/ray/curriculum.py
@@ -2,6 +2,8 @@
 # Ray example:
 # https://github.com/ray-project/ray/blob/master/rllib/examples/curriculum_learning.py
 # https://github.com/ray-project/ray/blob/master/rllib/examples/env/curriculum_capable_env.py#L9
+# https://github.com/ray-project/ray/blob/master/rllib/examples/custom_metrics_and_callbacks.py
+# https://github.com/ray-project/ray/blob/master/rllib/evaluation/episode_v2.py#L26
 
 # Standard Library Imports
 import random
@@ -9,9 +11,15 @@ from copy import deepcopy
 from pprint import pprint
 
 # Third Party Imports
+from numpy import abs, min
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+
 # %% Imports
 from ray.rllib.env.apis.task_settable_env import TaskSettableEnv, TaskType
+from ray.rllib.env.base_env import BaseEnv
 from ray.rllib.env.env_context import EnvContext
+from ray.rllib.evaluation.episode import Episode
+from ray.rllib.policy import Policy
 from ray.rllib.utils.annotations import override
 
 # Punch Clock Imports
@@ -19,10 +27,13 @@ from punchclock.ray.build_env import buildEnv
 
 
 # %% Curriculum function
-def curriculum_fn(
+def curriculumFnCustody(
     train_results: dict, task_settable_env: TaskSettableEnv, env_ctx: EnvContext
 ) -> TaskType:
     """Function returning a possibly new task to set `task_settable_env` to.
+
+    Increases level of difficulty based on mean percent targets in custody at end
+    of episode.
 
     Args:
         train_results: The train results returned by Algorithm.train().
@@ -37,43 +48,71 @@ def curriculum_fn(
         TaskType: The task to set the env to. This may be the same as the
             current one.
     """
-    # Our env supports tasks 1 (default) to 6.
-    # With each task, rewards get scaled up by a factor of 10, such that:
-    # Level 1: Expect rewards between 0.0 and 1.0.
-    # Level 2: Expect rewards between 1.0 and 10.0, etc..
-    # We will thus raise the level/task each time we hit a new power of 10.0
-    # new_task = int(np.log10(train_results["episode_reward_mean"]) + 2.1)
-    cur_level = task_settable_env.get_task()
     pprint(f"{train_results}")
+    cur_level = task_settable_env.get_task()
     print(f"current level (curriculum_fn) = {cur_level}")
-    if train_results["episode_reward_mean"] > 1:
-        new_task = cur_level + 1
-    else:
-        new_task = cur_level
+
+    percent_custody = (
+        train_results["custom_metrics"]["mean_last_sum_custody"]
+        / train_results["custom_metrics"]["num_targets"]
+    )
+    percent_custody_levels = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+    closest_percent = min(
+        percent_custody_levels, key=lambda x: abs(x - percent_custody)
+    )
+    new_task = percent_custody_levels.index(closest_percent)
 
     # Clamp between valid values, just in case:
-    new_task = max(min(new_task, 6), 1)
+    new_task = max(min(new_task, len(percent_custody_levels)), 1)
     print(
         f"Worker #{env_ctx.worker_index} vec-idx={env_ctx.vector_index}"
         f"\nR={train_results['episode_reward_mean']}"
         f"\nSetting env to task={new_task}"
+        f"\nPercent targets in custody = {percent_custody}"
+        f"\nNearest custody level = {closest_percent}"
     )
     return new_task
 
 
+# %% Custom callback
+class CustomCallbacks(DefaultCallbacks):
+    """Functions to be called throughout training."""
+
+    def on_episode_end(
+        self,
+        *,
+        worker: "RolloutWorker",  # noqa
+        base_env: BaseEnv,
+        policies: dict[str, Policy],
+        episode: Episode,
+        env_index: int,
+        **kwargs,
+    ):
+        """Record last custody sum in every episode."""
+        pprint(f"episode vars = {vars(episode)}")
+        last_info = episode._last_infos
+        pprint(f"\n {last_info=}")
+        # last_info is a 2-item dict, where the 0th item is empty and the 1st item
+        # is info returned from env.
+        last_info1 = list(last_info.values())[1]
+        pprint(f"\n {last_info1=}")
+        episode.custom_metrics["last_sum_custody"] = last_info1["sum_custody"]
+
+
 # %% Curriculum Wrapper
-class CurriculumCapableEnv(TaskSettableEnv):
+class CurriculumCustodyEnv(TaskSettableEnv):
     """Curriculum learning wrapper around SSAScheduler.
 
-    This simply wraps a SSAScheduler env and makes it harder with each
-    task. Task (difficulty levels) can range from 1 to 6."""
+    Wrap a SSAScheduler env and increase horizon with difficult level. Task (difficulty
+    levels) can range from 1 to 6.
+    """
 
     # Increase horizon for each level of difficulty
-    MAPS = [50, 100, 150, 200, 250, 288]
+    MAP = [50, 100, 150, 200, 250, 288]
 
     def __init__(self, config: EnvContext):
         self.cur_level = config.get("start_level", 1)
-        self.horizon = config.get("horizon", 10)
+        self.horizon = config.get("horizon", self.MAP[0])
         self.backup_config = deepcopy(config)
         self.env = None
         self._makeEnv(config)  # create self.env
@@ -89,17 +128,10 @@ class CurriculumCapableEnv(TaskSettableEnv):
         self._timesteps = 0
         return self.env.reset(seed=seed, options=options)
 
-    def step(self, action):
-        self._timesteps += 1
-        obs, rew, terminated, truncated, info = self.env.step(action)
-        # Make rewards scale with the level exponentially:
-        # Level 1: x1
-        # Level 2: x10
-        # Level 3: x100, etc..
-        rew *= 10 ** (self.cur_level - 1)
-        if self._timesteps >= self.horizon:
-            terminated = True
-        return obs, rew, terminated, truncated, info
+    # def step(self, action):
+    #     self._timesteps += 1
+    #     obs, rew, terminated, truncated, info = self.env.step(action)
+    #     return obs, rew, terminated, truncated, info
 
     @override(TaskSettableEnv)
     def sample_tasks(self, n_tasks):
@@ -119,7 +151,7 @@ class CurriculumCapableEnv(TaskSettableEnv):
 
     def _makeEnv(self, config: dict):
         new_config = deepcopy(config)
-        new_config["horizon"] = self.MAPS[self.cur_level - 1]
+        new_config["horizon"] = self.MAP[self.cur_level - 1]
         print(f"{self.cur_level=}")
         print(f"horizon = {new_config['horizon']}")
         del new_config["start_level"]  # prevents error in buildEnv caused by
--- tests/ray/test_curriculum.py
+++ tests/ray/test_curriculum.py
@@ -5,26 +5,35 @@ from pathlib import Path
 
 # Third Party Imports
 import ray
+from gymnasium.spaces import Box, Dict
 from ray import air, tune
+from ray.rllib.examples.env.random_env import RandomEnv
 from ray.rllib.utils import check_env
 from ray.tune.registry import get_trainable_cls, register_env
 
 # Punch Clock Imports
 from punchclock.common.utilities import loadJSONFile
+from punchclock.environment.env_parameters import SSASchedulerParams
+from punchclock.environment.misc_wrappers import RandomInfo
 from punchclock.ray.build_env import buildEnv
-from punchclock.ray.curriculum import CurriculumCapableEnv, curriculum_fn
+from punchclock.ray.curriculum import (
+    CurriculumCustodyEnv,
+    CustomCallbacks,
+    curriculumFnCustody,
+)
 
+# %% Test Env
+# Need to generate a random SSAScheduler env here, just he base env.
+config = SSASchedulerParams(horizon=10, agent_params={}, filter_params={})
+
+env_base = RandomInfo(
+    RandomEnv(),
+    info_space=Dict({"sum_custody": Box(low=0, high=10, shape=(), dtype=int)}),
+)
 # %% Tests
-register_env("ssa_env", buildEnv)
+# env = CurriculumCustodyEnv(env_config)
 
-config_path = Path(__file__).parent.joinpath("config_test").with_suffix(".json")
-loaded_config = loadJSONFile(config_path)
-env_config = loaded_config["param_space"]["env_config"]
-env_config.update({"start_level": 1})
-
-env = CurriculumCapableEnv(env_config)
-
-check_env(env)
+# check_env(env)
 
 ray.init(num_cpus=3, num_gpus=0)
 
@@ -32,9 +41,9 @@ config = (
     get_trainable_cls("PPO")
     .get_default_config()  # or "curriculum_env" if registered above
     .environment(
-        CurriculumCapableEnv,
+        CurriculumCustodyEnv,
         env_config=env_config,
-        env_task_fn=curriculum_fn,
+        env_task_fn=curriculumFnCustody,
     )
     .framework("torch")
     # .rollouts(num_rollout_workers=1, num_envs_per_worker=1)
