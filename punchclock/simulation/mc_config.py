8a9c5eb085002204dfc858a6704272a33967ce71
Dylan Penn <dylanrpenn@vt.edu>
2023-11-03 14:29:21 -0400

    allow Path object inputs to save

--- punchclock/simulation/mc_config.py
+++ punchclock/simulation/mc_config.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 # Standard Library Imports
 from datetime import datetime
+from pathlib import Path
 
 # Punch Clock Imports
 from punchclock.common.utilities import saveJSONFile
@@ -28,23 +29,23 @@ class MonteCarloConfig:
         """Initialize MC config.
 
         Args:
-        num_episodes (`int`): Number of episodes per trial.
-        policy_configs (`list[dict | str]`): One trial will be run for each
+        num_episodes (int): Number of episodes per trial.
+        policy_configs (list[dict | str]): One trial will be run for each
             entry in policies. If entry is a dict, must conform to buildCustomPolicy
             (see policy_builder.py). If entry is a str, a RayPolicy will be
             loaded from the checkpoint specified by the arg.
-        env_config (`dict`): See buildEnv.
-        results_dir (`str`): Where to save trial results. One DataFrame per
+        env_config (dict): See buildEnv.
+        results_dir (str): Where to save trial results. One DataFrame per
             trial will be saved here. Each file will have a unique name.
-        trial_names (`list`, optional): Names of trials. If not specified,
+        trial_names (list, optional): Names of trials. If not specified,
             trials are assigned integer names starting at 0.
-        print_status (`bool`, optional): Set to True to have status print while
+        print_status (bool, optional): Set to True to have status print while
             running simulations. Defaults to False.
-        num_cpus (`int`, optional): Number of cpus to make available when running
+        num_cpus (int, optional): Number of cpus to make available when running
             MC. Defaults to max available.
-        multiprocess (`bool`, optional): Whether or not to multiprocess trials
+        multiprocess (bool, optional): Whether or not to multiprocess trials
             during sim run. Used for debugging. Defaults to False.
-        single_sim_mode (`bool`, optional): Whether or not to use
+        single_sim_mode (bool, optional): Whether or not to use
             identical initial conditions between trials. If True, num_episodes
             must be 1, and all steps from all trials will be saved. Defaults
             to False.
@@ -77,19 +78,23 @@ class MonteCarloConfig:
 
     def save(
         self,
-        fpath: str,
+        fpath: str | Path,
         append_timestamp: bool = False,
     ):
         """Save a Monte Carlo config as a JSON file.
 
         Args:
-            fpath (`str`): Exclude file extension
-            append_timestamp (`bool`, optional): Append a time stamp to end of
+            fpath (str | Path): Exclude file extension
+            append_timestamp (bool, optional): Append a time stamp to end of
                 fpath. Defaults to False.
         """
+        if isinstance(fpath, str):
+            fpath = Path(fpath)
+
         if append_timestamp is True:
             now = datetime.now()
             date_time = now.strftime("%Y%m%d-%H%M%S")
-            fpath = fpath + "_" + date_time
+            # fpath = fpath + "_" + date_time
+            fpath = fpath.joinpath("_" + date_time)
 
-        saveJSONFile(fpath, self.config)
+        saveJSONFile(str(fpath), self.config)
