a1a4e0e6be544d9d4f6394f2e9bc4d86e9820e7e
Dylan Penn <dylanrpenn@vt.edu>
2023-11-06 10:36:09 -0500

    deleted old functions/classes in favor of ConfigurableX #96

--- punchclock/ray/curriculum.py
+++ punchclock/ray/curriculum.py
@@ -135,58 +135,6 @@ class ConfigurableCurriculumFn:
         return chainedGet(train_results, *self.metric, default=None)
 
 
-# %% Curriculum function
-def curriculumFnCustody(
-    train_results: dict, task_settable_env: TaskSettableEnv, env_ctx: EnvContext
-) -> TaskType:
-    """Function returning a possibly new task to set `task_settable_env` to.
-
-    Increases level of difficulty based on mean percent targets in custody at end
-    of episode.
-
-    Args:
-        train_results: The train results returned by Algorithm.train().
-        task_settable_env: A single TaskSettableEnv object
-            used inside any worker and at any vector position. Use `env_ctx`
-            to get the worker_index, vector_index, and num_workers.
-        env_ctx: The env context object (i.e. env's config dict
-            plus properties worker_index, vector_index and num_workers) used
-            to setup the `task_settable_env`.
-
-    Returns:
-        TaskType: The task to set the env to. This may be the same as the
-            current one.
-    """
-    pprint(f"{train_results}")
-    cur_level = task_settable_env.get_task()
-    print(f"current level (curriculum_fn) = {cur_level}")
-
-    num_targets = task_settable_env.env.num_targets
-    percent_custody = (
-        train_results["custom_metrics"]["last_custody_sum_mean"] / num_targets
-    )
-    percent_custody_levels = [0.1, 0.15, 0.2, 0.25, 0.3, 0.4]
-    closest_percent, idx = findNearest(
-        x=percent_custody_levels,
-        val=percent_custody,
-        round="down",
-        return_index=True,
-    )
-    new_task = idx
-
-    # Clamp between valid values, just in case:
-    new_task = max([min([new_task, len(percent_custody_levels)]), 1])
-
-    print(
-        f"Worker #{env_ctx.worker_index} vec-idx={env_ctx.vector_index}"
-        f"\nR={train_results['episode_reward_mean']}"
-        f"\nSetting env to task={new_task}"
-        f"\nPercent targets in custody = {percent_custody}"
-        f"\nNearest custody level = {closest_percent}"
-    )
-    return new_task
-
-
 # %% Custom callback
 class CustomCallbacks(DefaultCallbacks):
     """Functions to be called throughout training."""
@@ -218,68 +166,6 @@ class CustomCallbacks(DefaultCallbacks):
         episode.custom_metrics["last_custody_percent"] = last_info1[
             "custody_sum"
         ] / len(last_info1["custody"])
-
-
-# %% Curriculum Wrapper
-class CurriculumCustodyEnv(TaskSettableEnv):
-    """Curriculum learning wrapper around SSAScheduler.
-
-    Wrap a SSAScheduler env and increase horizon with difficult level. Task (difficulty
-    levels) can range from 1 to 6.
-    """
-
-    # Increase horizon for each level of difficulty
-    MAP = [50, 100, 150, 200, 250, 288]
-
-    def __init__(self, config: EnvContext):
-        self.cur_level = config.get("start_level", 1)
-        self.horizon = config.get("horizon", self.MAP[0])
-        self.backup_config = deepcopy(config)
-        self.env = None
-        self._makeEnv(config)  # create self.env
-        self.observation_space = self.env.observation_space
-        self.action_space = self.env.action_space
-        self.switch_env = False
-        self._timesteps = 0
-
-    def reset(self, *, seed=None, options=None):
-        if self.switch_env:
-            self.switch_env = False
-            self._makeEnv(self.backup_config)
-        self._timesteps = 0
-        return self.env.reset(seed=seed, options=options)
-
-    def step(self, action):
-        self._timesteps += 1
-        obs, rew, terminated, truncated, info = self.env.step(action)
-        return obs, rew, terminated, truncated, info
-
-    def _makeEnv(self, config: dict):
-        new_config = deepcopy(config)
-        new_config["horizon"] = self.MAP[self.cur_level - 1]
-        print(f"{self.cur_level=}")
-        print(f"horizon = {new_config['horizon']}")
-
-        # prevents error in buildEnv caused by unrecognized arg
-        new_config.pop("start_level", None)
-
-        self.env = buildEnv(new_config)
-
-    @override(TaskSettableEnv)
-    def sample_tasks(self, n_tasks):
-        """Implement this to sample n random tasks."""
-        return [random.randint(1, 10) for _ in range(n_tasks)]
-
-    @override(TaskSettableEnv)
-    def get_task(self):
-        """Implement this to get the current task (curriculum level)."""
-        return self.cur_level
-
-    @override(TaskSettableEnv)
-    def set_task(self, task):
-        """Implement this to set the task (curriculum level) for this env."""
-        self.cur_level = task
-        self.switch_env = True
 
 
 # %% ConfigurableCirriculumEnv
--- tests/ray/test_curriculum.py
+++ tests/ray/test_curriculum.py
@@ -15,9 +15,7 @@ from punchclock.ray.build_env import buildEnv
 from punchclock.ray.curriculum import (
     ConfigurableCurriculumEnv,
     ConfigurableCurriculumFn,
-    CurriculumCustodyEnv,
     CustomCallbacks,
-    curriculumFnCustody,
 )
 
 # %% Test Env
@@ -50,29 +48,20 @@ env = buildEnv(env_config)
 env.reset()
 env.step(env.action_space.sample())
 check_env(env)
-# %% Test CurriculumCustodyEnv
-print("\nTest CurriculumCustodyEnv...")
-taskable_env = CurriculumCustodyEnv(env_config)
-obs, info = taskable_env.reset()
+
+# %% Test ConfigurableCurriculumEnv
+env = ConfigurableCurriculumEnv(config=env_config)
+obs, info = env.reset()
 print(f"obs (reset) = {obs}")
 print(f"info (reset) = {info}")
 
-obs, rew, _, _, info = taskable_env.step(taskable_env.action_space.sample())
+obs, rew, _, _, info = env.step(env.action_space.sample())
 print(f"obs (step) = {obs}")
 print(f"info (step) = {info}")
 
-# %% Test curriculumFnCustody
-print("\nTest curriculumFnCustody...")
-results = {
-    "custom_metrics": {
-        "last_custody_sum_mean": 1.2,
-    },
-    "episode_reward_mean": 1,
-}
-env_ctx = EnvContext(env_config=env_config, worker_index=0)
-task = curriculumFnCustody(
-    train_results=results, task_settable_env=taskable_env, env_ctx=env_ctx
-)
+env.set_task({"horizon": 42})
+env.reset()
+task = env.get_task()
 print(f"{task=}")
 
 # %% Test ConfigurableCurriculumFn
@@ -82,15 +71,15 @@ c = ConfigurableCurriculumFn(
     metric_levels=[0.2, 0.5],
     task_map=task_map,
 )
-task = c(train_results=results, task_settable_env=taskable_env, env_ctx=env_ctx)
+results = {
+    "custom_metrics": {
+        "last_custody_sum_mean": 1.2,
+    },
+    "episode_reward_mean": 1,
+}
+env_ctx = EnvContext(env_config=env_config, worker_index=0)
+task = c(train_results=results, task_settable_env=env, env_ctx=env_ctx)
 print(f"{task=}")
-
-# %% Test ConfigurableCurriculumEnv
-env = ConfigurableCurriculumEnv(config=env_config)
-env.set_task({"horizon": 42})
-env.reset()
-task = env.get_task()
-
 
 # %% Test Fit
 ray.init(num_cpus=3, num_gpus=0)
