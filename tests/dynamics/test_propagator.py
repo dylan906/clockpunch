37a6266e5614e7fd40aeda6040ce3e535595cec6
Dylan Penn <dylanrpenn@vt.edu>
2023-11-14 12:31:11 -0500

    addressing linting

--- tests/dynamics/test_propagator.py
+++ tests/dynamics/test_propagator.py
@@ -12,7 +12,7 @@ from punchclock.dynamics.propagator import simplePropagate
 print("\nTest simple functionality...")
 
 
-def dummyFunc(t, x0):
+def dummyFunc(t, x0):  # noqa
     xdot = array([1, 1, -0.1])
     return xdot
 
@@ -24,14 +24,14 @@ print(f"propagated state=\n{x1}")
 print("\nTest t0=tf...")
 try:
     x1 = simplePropagate(dummyFunc, array([0, 0, 0]), 0, 0)
-except:
+except Exception:
     print("An exception occurred")
 
 # %% Test with IVP failure
 print("\nTest if IVP fails...")
 
 
-def dummyFunc2(t, x0):
+def dummyFunc2(t, x0):  # noqa
     xdot = [0, 0]
     if x0[0] < 0:
         xdot[0] = 1e15
@@ -45,7 +45,7 @@ def dummyFunc2(t, x0):
 try:
     x1 = simplePropagate(dummyFunc2, array([1e15, 1e15]), 0, 5)
     print(f"propagated state=\n{x1}")
-except:
+except Exception:
     print("An exception occurred")
 
 # %% Test with 2D initial conditions
