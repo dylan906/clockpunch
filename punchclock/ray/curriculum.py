51a2ae9620136e75a3ff77b86f90e611cad88976
Dylan Penn <dylanrpenn@vt.edu>
2023-11-25 15:27:19 -0500

    replaced del with pop #103

--- punchclock/ray/curriculum.py
+++ punchclock/ray/curriculum.py
@@ -82,7 +82,7 @@ class CustomCallbacks(DefaultCallbacks):
             "curriculum_metric_threshold_min",
             "curriculum_metric_threshold_max",
         ]:
-            del result["custom_metrics"][k]
+            result.pop(["custom_metrics"][k], None)
 
 
 # %% ConfigurableCurriculumFnV2
