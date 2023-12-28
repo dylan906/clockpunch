7d35f93c9f9197b19839b9cd270a1d021caadaa5
Dylan Penn <dylanrpenn@vt.edu>
2023-12-28 15:39:02 -0500

    better test for getRadialRate #107

--- tests/common/test_orbits.py
+++ tests/common/test_orbits.py
@@ -2,10 +2,11 @@
 # %% Imports
 # Third Party Imports
 from numpy import array
+from numpy.linalg import norm
 
 # Punch Clock Imports
 from punchclock.common.constants import getConstants
-from punchclock.common.orbits import getRadialRate
+from punchclock.common.orbits import getCircOrbitVel, getRadialRate
 
 RE = getConstants()["earth_radius"]
 MU = getConstants()["mu"]
@@ -15,6 +16,12 @@ print("\nTest getRadialRate...")
 r_vec = array([RE + 400, 0, 0])
 v_vec = array([1, 8, 0])
 
+rdot = getRadialRate(r_vec, v_vec)
+print(f"{rdot=}")
+
+# test with circular orbit (rdot=0)
+v = getCircOrbitVel(r=norm(r_vec))
+v_vec = array([0, v, 0])
 rdot = getRadialRate(r_vec, v_vec)
 print(f"{rdot=}")
 
