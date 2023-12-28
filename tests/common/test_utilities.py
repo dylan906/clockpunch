c5e452890bb8ade89939a78a1ac5f835a9e89f5c
Dylan Penn <dylanrpenn@vt.edu>
2023-12-28 14:35:38 -0500

    moved visibility tests to own module #107

--- tests/common/test_utilities.py
+++ tests/common/test_utilities.py
@@ -10,8 +10,6 @@ from numpy.random import randint
 from punchclock.common.agents import Sensor, Target
 from punchclock.common.utilities import (
     MaskConverter,
-    calcVisMap,
-    calcVisMapAndDerivative,
     findNearest,
     fromStringArray,
     isActionValid,
@@ -73,67 +71,6 @@ print(f"act_mask_1d = {act_mask_1d}")
 mask_no_inaction = mc.removeInactionsFrom1dMask(mask1d=act_mask_1d)
 print(f"mask without inaction = {mask_no_inaction}")
 
-# %% CalcVisMap
-RE = 6378
-sensor_states = array(
-    [
-        [RE + 100, 0, 0, 0, 0, 0],
-        [-RE - 100, 0, 0, 0, 0, 0],
-    ],
-).T
-target_states = array(
-    [
-        [RE + 200, 0, 0, 0, 1, 0],
-        [-RE - 200, 0, 0, 0, 0, 1],
-        [0, RE + 0.1, 0, 1, 0, 0],
-        [0, -RE - 0.1, 0, 1, 0, 0],
-    ],
-).T
-
-# test correct inputs
-vis_map = calcVisMap(
-    sensor_states=sensor_states,
-    target_states=target_states,
-    body_radius=RE,
-)
-print(f"vis_map = \n{vis_map}")
-
-vis_map = calcVisMap(
-    sensor_states=sensor_states,
-    target_states=target_states,
-    body_radius=RE,
-    binary=False,
-)
-print(f"vis_map = \n{vis_map}")
-
-# test with incorrect dimensions
-try:
-    vis_map = calcVisMap(
-        sensor_states=sensor_states.T,
-        target_states=target_states,
-        body_radius=RE,
-    )
-except Exception as err:
-    print(err)
-    pass
-
-try:
-    vis_map = calcVisMap(
-        sensor_states=sensor_states,
-        target_states=target_states.T,
-        body_radius=RE,
-    )
-except Exception as err:
-    print(err)
-    pass
-
-# %% Test calcVisMapAndDerivative
-print("\nTest calcVisMapAndDerivative()...")
-vis_map, vis_map_der = calcVisMapAndDerivative(
-    sensor_states=sensor_states, target_states=target_states, body_radius=RE
-)
-print(f"{vis_map=}")
-print(f"{vis_map_der=}")
 
 # %% Test printNestedDict
 print("\nTest printNestedDict()...")
--- tests/common/test_visibility.py
+++ tests/common/test_visibility.py
@@ -1,1 +1,68 @@
 """Tests for visibility.py."""
+# Third Party Imports
+from numpy import array
+
+# Punch Clock Imports
+from punchclock.common.visibility import calcVisMap, calcVisMapAndDerivative
+
+# %% CalcVisMap
+RE = 6378
+sensor_states = array(
+    [
+        [RE + 100, 0, 0, 0, 0, 0],
+        [-RE - 100, 0, 0, 0, 0, 0],
+    ],
+).T
+target_states = array(
+    [
+        [RE + 200, 0, 0, 0, 1, 0],
+        [-RE - 200, 0, 0, 0, 0, 1],
+        [0, RE + 0.1, 0, 1, 0, 0],
+        [0, -RE - 0.1, 0, 1, 0, 0],
+    ],
+).T
+
+# test correct inputs
+vis_map = calcVisMap(
+    sensor_states=sensor_states,
+    target_states=target_states,
+    body_radius=RE,
+)
+print(f"vis_map = \n{vis_map}")
+
+vis_map = calcVisMap(
+    sensor_states=sensor_states,
+    target_states=target_states,
+    body_radius=RE,
+    binary=False,
+)
+print(f"vis_map = \n{vis_map}")
+
+# test with incorrect dimensions
+try:
+    vis_map = calcVisMap(
+        sensor_states=sensor_states.T,
+        target_states=target_states,
+        body_radius=RE,
+    )
+except Exception as err:
+    print(err)
+    pass
+
+try:
+    vis_map = calcVisMap(
+        sensor_states=sensor_states,
+        target_states=target_states.T,
+        body_radius=RE,
+    )
+except Exception as err:
+    print(err)
+    pass
+
+# %% Test calcVisMapAndDerivative
+print("\nTest calcVisMapAndDerivative()...")
+vis_map, vis_map_der = calcVisMapAndDerivative(
+    sensor_states=sensor_states, target_states=target_states, body_radius=RE
+)
+print(f"{vis_map=}")
+print(f"{vis_map_der=}")
